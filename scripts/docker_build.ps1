a9222639548e5ff62f27ed622f5811cf8d722194
Haflandy <10549427+haflandy@users.noreply.github.com>
2025-12-08 19:15:02 +0100

    docker image build script

---
+++ scripts/docker_build.ps1
@@ -0,0 +1,10 @@
+# Build the image and and push it to Docker Hub
+# Login to Docker Hub must be done in advance with docker login
+
+$img = "haflandy/docker-duplicity"
+$tag = Get-Date -Format "yyyyMMddHHmm"
+
+& docker buildx build . -t ${img}:$tag
+& docker push ${img}:$tag
+& docker image tag ${img}:$tag ${img}:latest
+& docker push ${img}:latest
